25cf64d36431613d6ca29babc0dde8cec986d9b5
Avery Vanacore <avery.e.vanacore.20@dartmouth.edu>
2019-04-25 15:36:29 -0400

    updated starterpack

--- src/index.js
+++ src/index.js
@@ -1,11 +1,53 @@
-import $ from 'jquery';
 import './style.scss';
+import {
+  BrowserRouter as Router, Route, NavLink, Switch,
+} from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-$('#main').html('Here we go!');
+const About = (props) => {
+  return <div> All there is to know about me </div>;
+};
+const Welcome = (props) => {
+  return <div>Welcome</div>;
+};
 
-let secondCount = 0;
+const Test = (props) => {
+  return <div> ID: {props.match.params.id} </div>;
+};
 
-setInterval(() => {
-  secondCount += 1;
-  $('#main').html(`You've been on this page for ${secondCount} seconds.`);
-}, 1000);
+const FallBack = (props) => {
+  return <div>URL Not Found</div>;
+};
+
+const Nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/" exact>Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
+
+const App = (props) => {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        {/* eslint-disable-next-line react/jsx-no-undef */}
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/about" component={About} />
+          <Route exact path="/test/:id" component={Test} />
+          <Route component={FallBack} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('main'));
